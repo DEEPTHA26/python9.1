240edb8c8dc0d33426c4da4c84a35aab7fe33f95
DEEPTHA26 <51949707+DEEPTHA26@users.noreply.github.com>
2019-06-30 19:42:22 +0530

    palli.py

--- palli.py
+++ palli.py
@@ -1,11 +1,5 @@
-N=int(input())
-temp=N
-pallindrome=0
-while N!=0:
-  number=N%10
-  pallindrome=pallindrome*10+number
-  N=N//10
-if temp==pallindrome:
+numbers=input()
+if(numbers==numbers[::-1]):
   print("yes")
 else:
-print("no")
+  print("no")
