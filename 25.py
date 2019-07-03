29f3ff71c2f825abc505cb81db0e7ae2a34910b8
DEEPTHA26 <51949707+DEEPTHA26@users.noreply.github.com>
2019-07-03 11:30:13 +0530

    Update and rename 25.py to 25d.py

--- 25.py
+++
@@ -1,4 +0,0 @@
-num1,num2=map(int,input().split())
-for d in range(num1+1,num2):
-if(d%2==0):
-  print(d,end="")
---
+++ 25d.py
@@ -0,0 +1,4 @@
+nu1,nu2=map(int,input().split())
+for d1 in range(nu1+1,nu2):
+  if(d1%2==0):
+    print(d1,end="")
